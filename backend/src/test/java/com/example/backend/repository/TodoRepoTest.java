d55df79e87393021d5874119ad6de46f021875d1
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-03-26 22:21:02 +0530

    adding Status Update Test Case

--- backend/src/test/java/com/example/backend/repository/TodoRepoTest.java
+++ backend/src/test/java/com/example/backend/repository/TodoRepoTest.java
@@ -1,10 +1,11 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.TodoModel;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +19,10 @@ class TodoRepoTest {
     @Autowired
     private TodoRepo underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
     @Test
     void itShouldCheckTop05IncompletedTasks() {
         //given
@@ -28,7 +33,8 @@ class TodoRepoTest {
                 new TodoModel("D","Description D",true),
                 new TodoModel("F","Description F",false),
                 new TodoModel("G","Description G",true),
-                new TodoModel("H","Description H",false)
+                new TodoModel("H","Description H",false),
+                new TodoModel("I","Description I",false)
         );
         underTest.saveAll(todos);
 
@@ -37,7 +43,32 @@ class TodoRepoTest {
 
         //then
         assertThat(taskList).hasSize(5);
-        assertThat(taskList.get(0).getTitle()).isEqualTo("H");
-        assertThat(taskList.get(3).getTitle()).isEqualTo("B");
+        assertThat(taskList.get(0).getTitle()).isEqualTo("I");
+        assertThat(taskList.get(3).getTitle()).isEqualTo("C");
+        assertThat(taskList.get(4).getTitle()).isEqualTo("B");
+    }
+
+    @Test
+    void itShouldCheckEmptyIncompletedTasks() {
+        //given
+
+        //when
+        List<TodoModel> taskList=underTest.getTodoList();
+
+        //then
+        assertThat(taskList).hasSize(0);
+    }
+
+    @Test
+    void itShouldCheckCompletedStatusUpdate(){
+        List<TodoModel> todos = Arrays.asList(
+                new TodoModel("A","Description A",false),
+                new TodoModel("B","Description B",false)
+        );
+        List<TodoModel> savedTodos=underTest.saveAll(todos);
+        long taskId=savedTodos.get(0).getTaskId();
+        int isUpdated=underTest.updateIsCompletedToTrue(taskId);
+        assertThat(isUpdated).isEqualTo(1);
+
     }
 }
